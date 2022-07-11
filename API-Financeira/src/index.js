96f3dea6c3ef32e0f442fcd1c74cfccebab0a0fd
Leticia Rodrigues <leticia.bento@fortbrasil.com.br>
2022-07-11 08:58:44 -0300

    Cadastro conta

--- API-Financeira/src/index.js
+++ API-Financeira/src/index.js
@@ -1,5 +1,20 @@
+const { request, response } = require("express");
+const { v4: uuidv4 } = require("uuid");
 const express = require("express");
-
+const customers = [];
 const app = express();
+app.use(express.json());
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
 
+  const id = uuidv4();
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: [],
+  });
+  return response.status(201).send();
+});
 app.listen(3333);
