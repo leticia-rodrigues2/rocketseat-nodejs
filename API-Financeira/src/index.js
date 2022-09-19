26427ab4b1f03115712cec41f798a687a1adbe9f
Leticia Rodrigues <leticiaarodrigues2@gmail.com>
2022-09-19 14:12:52 -0300

    remover conta

--- API-Financeira/src/index.js
+++ API-Financeira/src/index.js
@@ -109,4 +109,16 @@ app.get("/account", verifyIfExixtisAccountCPF, (request, response) => {
   return response.json(customer);
 });
 
+app.delete("/account", verifyIfExixtisAccountCPF, (request, response) => {
+  const { customer } = request;
+  customers.splice(customer, 1);
+  return response.status(200).json(customers);
+});
+
+app.get("/balance", verifyIfExixtisAccountCPF, (request, response) => {
+  const { customer } = request;
+  const balance = getBalance(customer.statement);
+
+  return response.json(balance);
+});
 app.listen(3333);
